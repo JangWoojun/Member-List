4eb514b3c89098edf2b144177852b9390a22f15c
JangWoojun <woojun0107@naver.com>
2023-02-05 22:04:12 +0900

    feat: NextView추가

---
+++ Member List/Views/NextView.swift
@@ -0,0 +1,20 @@
+//
+//  NextView.swift
+//  Member List
+//
+//  Created by Jangwoojun on 2023/02/05.
+//
+
+import UIKit
+
+class NextView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
