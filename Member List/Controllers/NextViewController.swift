070ac1c9311626651a27b5f496a03dc965ef01c7
JangWoojun <woojun0107@naver.com>
2023-02-05 22:03:33 +0900

    feat: NextViewControllers 추가

---
+++ Member List/Controllers/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  Member List
+//
+//  Created by Jangwoojun on 2023/02/05.
+//
+
+import UIKit
+
+final class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
